2a37f687e4f22937fc8bb94b68ccb806f78ec144
sergio-code <115664238+sergio-code@users.noreply.github.com>
2022-11-14 20:43:44 -0300

    Calculadora Version Final
    Agregado de memoria para el calculo de las Operaciones y bucle para continuar operando.

--- calculadora.py
+++ calculadora.py
@@ -6,12 +6,12 @@
 ############################
 #este programa inicial realiza una secuencia de codigos que permiten al Operador mejorar su resultado de busqueda.
 
-number1=int(input("agregar un numero:"))
-number2=int(input("agregar otro numero:"))
+number1=float(input("agregar un numero:"))
+number2=float(input("agregar otro numero:"))
 
 eleccion = 0
 
-while eleccion != 5:
+while eleccion != 6:
 
     print("""
     indique la operacion a realizar: 
@@ -20,28 +20,41 @@ while eleccion != 5:
     2) resta
     3) multiplicacion
     4) division
-    5) volver a inicio
+    5) seguir operando
+    6) volver a inicio
     """) 
        
-    eleccion = int(input())
+    eleccion = float(input())
 
     if eleccion == 1:
         print(" ")
         print("resultado: ", number1,"+", number2,"=", number1+number2)
-
+        numberanterior= number1+number2
+        
     if eleccion == 2:
         print("")
         print("resultado:", number1,"-", number2,"=", number1-number2)
+        numberanterior= number1-number2
 
     if eleccion == 3:
         print("")
         print("resultado:", number1,"*", number2,"=", number1*number2)
+        numberanterior= number1*number2
 
     if eleccion == 4:
         print("")
         print("resultado:", number1,"/", number2,"=", number1/number2)
+        numberanterior= number1/number2
 
     if eleccion == 5:
+        print ("seguir operando") 
+        number1 = numberanterior
+        number2=float(input("agregar otro numero:"))
+
+    if eleccion == 6:
         print("Gracias Totales")
 
         break
+    continue
+
+
