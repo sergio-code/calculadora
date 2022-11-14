f110bd046ee8057384191ed04fd10f0123b292c6
sergio-code <115664238+sergio-code@users.noreply.github.com>
2022-11-14 15:07:49 -0300

    Actualización Calculadora
    Agregado de funciones necesarias y corte final

--- calculadora.py
+++ calculadora.py
@@ -6,29 +6,42 @@
 ############################
 #este programa inicial realiza una secuencia de codigos que permiten al Operador mejorar su resultado de busqueda.
 
-number1=int(input("agregar un numero"))
-number2=int(input("agregar otro numero"))
+number1=int(input("agregar un numero:"))
+number2=int(input("agregar otro numero:"))
 
-eleccion=0
+eleccion = 0
 
-while eleccion != 3:
+while eleccion != 5:
 
     print("""
-    indique la operacion a realizar:
+    indique la operacion a realizar: 
+
     1) suma
     2) resta
-    3) volver a inicio
-    """)
+    3) multiplicacion
+    4) division
+    5) volver a inicio
+    """) 
+       
     eleccion = int(input())
 
     if eleccion == 1:
-        print("")
-        print("resultado:", number1,"+", number2,"=", number1+number2)
+        print(" ")
+        print("resultado: ", number1,"+", number2,"=", number1+number2)
+
     if eleccion == 2:
         print("")
         print("resultado:", number1,"-", number2,"=", number1-number2)
 
     if eleccion == 3:
+        print("")
+        print("resultado:", number1,"*", number2,"=", number1*number2)
+
+    if eleccion == 4:
+        print("")
+        print("resultado:", number1,"/", number2,"=", number1/number2)
+
+    if eleccion == 5:
         print("Gracias Totales")
 
-        
+        break
